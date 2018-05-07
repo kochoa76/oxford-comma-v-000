b7b8d8e5c4d5b02b868eb39f319c672500b15ad5
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-07 13:47:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,6 +7,8 @@ if array.length == 2
 return array.join(" and ")
 end
 
-  array[0..-2].join(", ") + " and " + array[-1]
+if array.length== 3
+return array[0..-2].join(",") + " and " + array[-1]
+end
 
 end
